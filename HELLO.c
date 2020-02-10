a67cc34db450337ab6d581eeb475b900fa1178c1
prasadparkar <prasad.parkar94@gmail.com>
2020-02-10 23:13:57 +0530

    version1

---
+++ HELLO.c
@@ -0,0 +1,8 @@
+//This is my first program in C language
+#include <stdio.h>
+int main()
+{
+printf ("HELLO WORLD");
+return 0;
+//The value 0 is used for successful execution and 1 for unsuccessful execution
+}
