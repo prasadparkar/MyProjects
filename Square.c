34f7f36d5edd0bbdf148734717b9969a77b90a42
prasadparkar <prasad.parkar94@gmail.com>
2020-02-10 23:39:59 +0530

    version1

---
+++ Square.c
@@ -0,0 +1,10 @@
+//This program gets input from user and prints its sqaure value.
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter a number:");
+scanf("%d",&number);
+printf("Square of number is:%d ",number*number);
+return 0;
+}
