862673f908eff5e8fe064d825125c23243bc8a37
prasadparkar <prasad.parkar94@gmail.com>
2020-02-11 20:10:12 +0530

    version1

--- Square.c
+++ Square.c
@@ -5,6 +5,7 @@ int main()
 int number;
 printf("enter a number:");
 scanf("%d",&number);
+// %d is used to print the signed integer value where signed integer means that the variable can hold both positive and negative values.
 printf("Square of number is:%d ",number*number);
 return 0;
 }
