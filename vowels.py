5478b18fefd35a3fd86e1dd42b194ea78d180a9f
prasadparkar <prasad.parkar94@gmail.com>
2020-02-23 19:59:15 +0530

    version1

---
+++ vowels.py
@@ -0,0 +1,18 @@
+#This program prints number of vowels present in a sentence.
+snt= input("Type a sentence here:")
+str= snt.lower()
+count= 0
+list1= ["a","e","i","o","u"]
+
+for char in str:
+    if char in list1:
+        count= count+1
+
+print("Number of vowels in given sentence:",count)
+
+"""
+OUTPUT:
+Type a sentence here:I love my India.
+Number of vowels in given sentence: 6
+
+"""s
